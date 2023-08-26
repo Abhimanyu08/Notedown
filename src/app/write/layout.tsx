d4bae17fcd32f5eca726f4d232ba7ec5f0492158
Abhimanyu. <iamabhimanyu08@gmail.com>
2023-08-26 12:46:40 +0530

    add all notes sidebar to write layout

--- src/app/write/layout.tsx
+++ src/app/write/layout.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import BlogContextProvider from "../../components/BlogPostComponents/BlogState";
 import EditorContextProvider from "./components/EditorContext";
+import SideSheet from "@components/SideSheet";
+import { Button } from "@components/ui/button";
+import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { cookies, headers } from "next/headers";
+import Link from "next/link";
 
-function WriteLayout({ children }: { children: React.ReactNode }) {
+async function WriteLayout({ children }: { children: React.ReactNode }) {
+	const supabase = createServerComponentSupabaseClient({ headers, cookies });
+	const {
+		data: { session },
+	} = await supabase.auth.getSession();
+
 	return (
 		<EditorContextProvider>
+			<SideSheet>
+				<Link
+					href={
+						session
+							? `/profile/${session.user.id}`
+							: `/profile/anon`
+					}
+				>
+					<Button>All Notes</Button>
+				</Link>
+			</SideSheet>
+
 			<BlogContextProvider>{children}</BlogContextProvider>
 		</EditorContextProvider>
 	);
