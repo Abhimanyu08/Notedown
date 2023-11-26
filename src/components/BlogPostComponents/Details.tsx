250a9498d83039884ab405cd93fe588f770e000d
Abhimanyu. <iamabhimanyu08@gmail.com>
2023-11-26 18:04:39 +0530

    remove details for now

--- src/components/BlogPostComponents/Details.tsx
+++ src/components/BlogPostComponents/Details.tsx
@@ -1,36 +1,36 @@
-"use client";
-import { mdToHast, transformer } from "@utils/html2Jsx/transformer";
-import { memo, useEffect, useState } from "react";
+// "use client";
+// import { mdToHast, transformer } from "@utils/html2Jsx/transformer";
+// import { memo, useEffect, useState } from "react";
 
-function Details({
-	summaryText,
-	detailsText,
-}: {
-	detailsText: string;
-	summaryText: string;
-}) {
-	const [summaryJsx, setSummaryJsx] = useState<JSX.Element>(<></>);
-	const [detailsJsx, setDetailsJsx] = useState<JSX.Element>(<></>);
+// function Details({
+// 	summaryText,
+// 	detailsText,
+// }: {
+// 	detailsText: string;
+// 	summaryText: string;
+// }) {
+// 	const [summaryJsx, setSummaryJsx] = useState<JSX.Element>(<></>);
+// 	const [detailsJsx, setDetailsJsx] = useState<JSX.Element>(<></>);
 
-	useEffect(() => {
-		// setSummaryText(st)
+// 	useEffect(() => {
+// 		// setSummaryText(st)
 
-		if (summaryText) {
-			setSummaryJsx(transformer(mdToHast(summaryText)["htmlAST"]));
-		}
-		if (detailsText) {
-			setDetailsJsx(transformer(mdToHast(detailsText)["htmlAST"]));
-		}
-	}, [summaryText, detailsText]);
+// 		if (summaryText) {
+// 			setSummaryJsx(transformer(mdToHast(summaryText)["htmlAST"]));
+// 		}
+// 		if (detailsText) {
+// 			setDetailsJsx(transformer(mdToHast(detailsText)["htmlAST"]));
+// 		}
+// 	}, [summaryText, detailsText]);
 
-	return (
-		<details className="marker:text-white py-3 px-4 rounded-sm border-[1px] border-gray-200">
-			<summary className="[&>p]:inline cursor-pointer text-lg ">
-				{summaryJsx}
-			</summary>
-			<section className="px-4">{detailsJsx}</section>
-		</details>
-	);
-}
+// 	return (
+// 		<details className="marker:text-white py-3 px-4 rounded-sm border-[1px] border-gray-200">
+// 			<summary className="[&>p]:inline cursor-pointer text-lg ">
+// 				{summaryJsx}
+// 			</summary>
+// 			<section className="px-4">{detailsJsx}</section>
+// 		</details>
+// 	);
+// }
 
-export default memo(Details);
+// export default memo(Details);
