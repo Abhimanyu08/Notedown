694bc2103ab9ea8eb98430e706d004f90d1a1e63
Abhimanyu. <iamabhimanyu08@gmail.com>
2023-11-28 19:09:52 +0530

    bug fix

--- src/components/ActionWrapper.tsx
+++ src/components/ActionWrapper.tsx
@@ -8,15 +8,18 @@ import {
 } from "./ui/menubar";
 import { SlOptions } from "react-icons/sl";
 import { usePathname, useSelectedLayoutSegment } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 function ActionWrapper({ children }: { children: React.ReactNode }) {
 	const pathname = usePathname();
 	return (
 		<Menubar
-			className={`absolute top-3 right-3 w-fit h-fit border-none
+			className={cn(
+				`absolute top-3 right-3 w-fit h-fit border-none
 					rounded-md bg-transparent hover:bg-secondary
-					${!(pathname?.includes("post") || pathname?.includes("draft")) ? "" : "hidden"}
-			`}
+			`,
+				!pathname?.startsWith("/notebook") && "hidden"
+			)}
 		>
 			<MenubarMenu>
 				<MenubarTrigger className="p-[2px] rounded-md">
