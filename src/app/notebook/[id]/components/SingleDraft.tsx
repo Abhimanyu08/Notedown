cf38c49bbb230991c6ed701ddd41a72fdeed4e6b
Abhimanyu. <iamabhimanyu08@gmail.com>
2023-11-29 17:13:09 +0530

    fixed draft link

--- src/app/notebook/[id]/components/SingleDraft.tsx
+++ src/app/notebook/[id]/components/SingleDraft.tsx
@@ -3,21 +3,25 @@ import { Draft } from "@utils/processDrafts";
 import Link from "next/link";
 import { DraftActions } from "./DraftActions";
 import { DraftOnPreviewIndicator } from "./DraftOnPreviewIndicator";
-import { useParams } from "next/navigation";
+import { useParams, usePathname, useSearchParams } from "next/navigation";
 
 export type SingleDraftProp = { draft: Draft; tag?: string };
 
 export function SingleDraft({ draft, tag }: SingleDraftProp) {
 	const { title } = draft;
-	const params = useParams();
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
+
+	const href = searchParams?.has("showtag")
+		? `${pathname}?showtag=${searchParams.get("showtag")}&draft=${
+				draft.timeStamp
+		  }&tag=${tag}`
+		: `${pathname}?draft=${draft.timeStamp}&tag=${tag}`;
 	return (
 		<div className="flex flex-col group p-2 relative ">
 			<DraftOnPreviewIndicator draftId={draft.timeStamp} tag={tag} />
 			<DraftActions draft={draft} tag={tag!} />
-			<Link
-				href={`/notebook/${params?.id}?draft=${draft.timeStamp}&tag=${tag}`}
-				className=""
-			>
+			<Link href={href} className="">
 				<PostTitle title={title || ""} />
 				<p className="text-xs text-gray-400 mt-1">
 					<span className="">{draft.date}</span>
