1ea5491ec3b08ff057cc59a45e262a61a31f6fbc
Abhimanyu. <iamabhimanyu08@gmail.com>
2023-12-14 16:26:17 +0530

    drag and drop; should've done it way earlier

--- src/components/EditorComponents/ImageUploader.tsx
+++ src/components/EditorComponents/ImageUploader.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { EditorContext } from "@/app/write/components/EditorContext";
-import { cn } from "@/lib/utils";
 import { IndexedDbContext } from "@/contexts/IndexedDbContext";
-import { ToolTipComponent } from "@components/ToolTipComponent";
+import { cn } from "@/lib/utils";
 import { Button } from "@components/ui/button";
-import { processImageName } from "@utils/makeFolderName";
-import React, { ChangeEventHandler, useContext } from "react";
-import { BiImageAdd } from "react-icons/bi";
 import { Input } from "@components/ui/input";
+import { processImageName } from "@utils/makeFolderName";
+import React, { useCallback, useContext } from "react";
+import { useDropzone } from "react-dropzone";
+import { BiImageAdd } from "react-icons/bi";
 
 function ImageUploader({
 	className,
@@ -21,55 +21,68 @@ function ImageUploader({
 	// const { dispatch } = useContext(BlogContext);
 	const { editorState, dispatch } = useContext(EditorContext);
 	const { documentDb } = useContext(IndexedDbContext);
+	const onDrop = useCallback(
+		(acceptedFiles: File[]) => {
+			// Do something with the files
+			if (!editorState || !documentDb) return;
+			if (acceptedFiles.length === 0) return;
+			const imagesMap: Record<string, File> = {};
+			for (let file of acceptedFiles) {
+				imagesMap[file.name] = file;
+			}
+			const { editorView, frontMatterLength } = editorState;
+			if (!editorView) return;
+			if (!end) return;
+			dispatch({
+				type: "add image to files",
+				payload: imagesMap,
+			});
+			for (let [imageName, imageFile] of Object.entries(imagesMap)) {
+				const imageBlob = imageFile;
+				let objectStore = documentDb!
+					.transaction("images", "readwrite")
+					.objectStore("images");
 
-	const onImageSelect: ChangeEventHandler<HTMLInputElement> = async (e) => {
-		const [imagesObj, fileNamesToAdd] = getImagesObj(e);
-		const { editorView, frontMatterLength } = editorState;
-		if (!editorView) return;
-		if (fileNamesToAdd.length === 0) return;
-		if (!end) return;
-		const imageNames = fileNamesToAdd.join(",");
-		dispatch({
-			type: "add image to files",
-			payload: imagesObj,
-		});
-		for (let [imageName, imageFile] of Object.entries(imagesObj)) {
-			const imageBlob = imageFile;
-			let objectStore = documentDb!
-				.transaction("images", "readwrite")
-				.objectStore("images");
+				const newData = {
+					imageName,
+					imageBlob,
+				};
 
-			const newData = {
-				imageName,
-				imageBlob,
-			};
-
-			objectStore.put(newData);
-		}
-		editorState.editorView?.focus();
-		// if we are adding to already uploaded images then we need to put a comma in front.
-		const toInsert = add ? "," + imageNames : imageNames;
-		editorState.editorView?.dispatch({
-			changes: {
-				from: end + frontMatterLength - 1,
-				insert: toInsert,
-			},
-		});
-	};
+				objectStore.put(newData);
+			}
+			editorState.editorView?.focus();
+			// if we are adding to already uploaded images then we need to put a comma in front.
+			const imageNames = Object.keys(imagesMap).join(",");
+			const toInsert = add ? "," + imageNames : imageNames;
+			editorState.editorView?.dispatch({
+				changes: {
+					from: end + frontMatterLength - 1,
+					insert: toInsert,
+				},
+			});
+		},
+		[documentDb, editorState]
+	);
+	const { getRootProps, getInputProps, isDragActive } = useDropzone({
+		onDrop,
+	});
 
 	if (add) {
 		return (
-			<>
+			<div {...getRootProps()} className="self-end ">
 				<input
 					type="file"
-					onChange={onImageSelect}
 					id={`add-images-${end}`}
 					accept="image/*"
 					className="hidden"
 					multiple
+					{...getInputProps()}
 				/>
 				<Button
-					className="text-sm bg-black py-1 px-2  self-end border-border border-[1px] mb-2 text-gray-400 hover:text-white hover:scale-100"
+					className={cn(
+						"text-sm bg-black py-1 px-2  border-border border-[1px] mb-2 text-gray-400 hover:text-white hover:scale-100",
+						isDragActive && "bg-secondary"
+					)}
 					variant={"outline"}
 				>
 					{/* <Button className="border-border border-[1px] text-gray-400 hover:text-white hover:scale-100"> */}
@@ -82,24 +95,26 @@ function ImageUploader({
 					</label>
 					{/* </Button> */}
 				</Button>
-			</>
+			</div>
 		);
 	}
 	return (
 		<div
 			className={cn(
 				"flex w-full justify-center py-10 items-center my-10 border-t-[1px] border-b-[1px] border-border",
-				className
+				className,
+				isDragActive && "bg-secondary"
 			)}
+			{...getRootProps()}
 		>
 			<Input
 				type="file"
-				onChange={onImageSelect}
 				// id="gallery-input"
 				accept="image/*"
 				className="hidden"
 				id={`upload-images-${end}`}
 				multiple
+				{...getInputProps()}
 			/>
 			<Button
 				className="text-sm bg-black self-end border-border border-[1px] mb-2 text-gray-400 hover:text-white hover:scale-100"
@@ -112,7 +127,7 @@ function ImageUploader({
 					htmlFor={`upload-images-${end}`}
 				>
 					<BiImageAdd size={12} />
-					<span>Upload Images</span>
+					<span>Upload / Drag Images</span>
 				</label>
 				{/* </Button> */}
 			</Button>
